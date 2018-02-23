46c174780fb3b9b44b9bd356599e2a33d166254c
Sergio <me@testing.com>
2018-02-23 17:57:26 +0100

    Product list first approach

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const Koa = require('koa');
 const app = new Koa();
+const Router = require('koa-router');
+const axios = require('axios');
+const { transform, checkParams, listURL } = require('./product-list');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World!!';
+let router = new Router();
+
+router.get('/list', async (ctx, next) => {
+  ctx.body = await checkParams(ctx.request.query)
+    .then(axios.get.bind(axios, listURL))
+    .then(transform.bind(null, ctx))
+    .catch(error => error);
+
+  next();
 });
+
+app.use(router.routes());
 
 if (!module.parent) {
   app.listen(4033);
