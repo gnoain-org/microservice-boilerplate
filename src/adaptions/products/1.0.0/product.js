04d539abca5a6d0dd23bc5e2d5c9d31b36247615
Sergio <me@testing.com>
2018-04-23 15:33:55 +0200

    Moonshine adaption sync.

--- src/adaptions/products/1.0.0/product.js
+++ src/adaptions/products/1.0.0/product.js
@@ -1,7 +1,13 @@
 const transformProduct = require('./trasformers/product.transformer');
+const R = require('ramda');
+
+const filterDeepByRecursive = require('../../../util/filterDeepByRecursive');
+const cleanObject = filterDeepByRecursive(
+  value => typeof value !== 'undefined' && !R.isEmpty(value)
+);
 
 const adaptProduct = ctx => {
-  ctx.response.body = transformProduct(ctx.response.body);
+  ctx.response.body = cleanObject(transformProduct(ctx.response.body));
 };
 
 module.exports = adaptProduct;
--- src/adaptions/products/1.0.0/trasformers/product.transformer.js
+++ src/adaptions/products/1.0.0/trasformers/product.transformer.js
@@ -1,12 +1,8 @@
 const _ = require('lodash');
 const R = require('ramda');
 
-const filterDeepBy = require('../../../../util/filterDeepBy');
-const cleanObject = filterDeepBy(value => typeof value !== 'undefined');
-
-const transformProduct = inputProduct => {
-  let input = inputProduct;
-
+var transformProduct = product => {
+  let input = product;
   const renameKeys = R.curry((keysMap, obj) =>
     R.reduce(
       (acc, key) => R.assoc(R.propOr(key, key, keysMap), obj[key], acc),
@@ -20,7 +16,11 @@ const transformProduct = inputProduct => {
       R.pathOr([], ['values', 'values']),
       R.ifElse(
         R.all(R.has('value')),
-        R.map(renameKeys({ value: 'text' })),
+        R.map(
+          renameKeys({
+            value: 'text'
+          })
+        ),
         R.chain(transformAttribute)
       )
     )(attribute);
@@ -43,7 +43,11 @@ const transformProduct = inputProduct => {
           R.is(String),
           R.applySpec({
             type: R.always(resourceKey),
-            links: [{ url: R.identity }]
+            links: [
+              {
+                url: R.identity
+              }
+            ]
           })
         ],
         [
@@ -75,7 +79,11 @@ const transformProduct = inputProduct => {
           R.applySpec({
             type: R.always('gif'),
             name: R.pipe(R.keys, R.head),
-            links: [{ url: R.pipe(R.values, R.head) }]
+            links: [
+              {
+                url: R.pipe(R.values, R.head)
+              }
+            ]
           })
         ],
         [
@@ -104,14 +112,46 @@ const transformProduct = inputProduct => {
         'product_type',
         'short_description',
         'store_id',
-        'title'
+        'title',
+        'mandatory_info'
       ],
       input
     )
   );
 
+  // Cross Selling
+  output.cross_selling = R.pipe(
+    R.propOr([], ['cross_selling']),
+    R.map(transformProduct)
+  )(input);
+
+  // Collection Skus
+  output.collection_skus = R.pipe(
+    R.propOr([], ['collection_skus']),
+    R.map(R.applySpec({ id: R.propOr('', 'product'), skus: R.of })),
+    R.map(transformProduct)
+  )(input);
+
+  // Parent Collection
+  output.parent_collection = R.pipe(
+    R.propOr([], ['parent_collection']),
+    R.map(transformProduct)
+  )(input);
+
+  // Range
+  output.range = R.pipe(
+    R.pathOr([], ['range', 'products']),
+    R.map(transformProduct),
+    R.assoc('products', input.range)
+  )(input);
+
   // Brand
-  output.brand = renameKeys({ value: 'id' }, input.brand);
+  output.brand = renameKeys(
+    {
+      value: 'id'
+    },
+    input.brand
+  );
 
   // Descriptions
   output.descriptions = input.description;
@@ -127,49 +167,9 @@ const transformProduct = inputProduct => {
     R.propOr(null, 'sku')
   )(input.skus);
 
-  //Recursive product adaption
-
-  input.collection_skus = R.when(
-    R.complement(R.isNil),
-    R.map(transformProduct)
-  )(input.collection_skus);
-  input.range = R.when(
-    R.pipe(R.prop('products'), R.complement(R.isNil)),
-    R.pipe(
-      R.prop('products'),
-      R.map(transformProduct),
-      R.assoc('products', input.range)
-    )
-  )(R.path(['range'], input));
-  input.cross_selling = R.when(R.complement(R.isNil), R.map(transformProduct))(
-    input.cross_selling
-  );
-  input.parent_collection = R.when(
-    R.complement(R.isNil),
-    R.map(transformProduct)
-  )(input.parent_collection);
-
   // Attribute groups
 
-  const createAttributeGroupFromProps = (name, attributesPaths, origin) => {
-    let group = {
-      group: name,
-      attributes: R.map(
-        R.applySpec({
-          name: R.last,
-          values: R.pipe(R.path(R.__, origin), R.of)
-        }),
-        attributesPaths
-      )
-    };
-    group.attributes = R.filter(
-      R.pipe(R.prop('values'), R.head, R.isNil, R.not),
-      group.attributes
-    );
-
-    return R.isEmpty(group.attributes) ? undefined : group;
-  };
-
+  // Attribute Groups :: Original
   output.attribute_groups = R.pipe(
     R.propOr([], 'attribute_groups'),
     R.reduce(
@@ -196,7 +196,10 @@ const transformProduct = inputProduct => {
         );
         return acc;
       },
-      { currentGroup: null, groups: {} }
+      {
+        currentGroup: null,
+        groups: {}
+      }
     ),
     R.prop('groups'),
     R.values,
@@ -209,6 +212,7 @@ const transformProduct = inputProduct => {
     })
   )(input);
 
+  // Attribute Groups :: Custom
   const attributesConfig = [
     ['food_attributes', [['food_sale_info'], ['food_information']]],
     [
@@ -239,6 +243,25 @@ const transformProduct = inputProduct => {
     ]
   ];
 
+  const createAttributeGroupFromProps = (name, attributesPaths, origin) => {
+    let group = {
+      group: name,
+      attributes: R.map(
+        R.applySpec({
+          name: R.last,
+          values: R.pipe(R.path(R.__, origin), R.of)
+        }),
+        attributesPaths
+      )
+    };
+    group.attributes = R.filter(
+      R.pipe(R.prop('values'), R.head, R.isNil, R.not),
+      group.attributes
+    );
+
+    return R.isEmpty(group.attributes) ? undefined : group;
+  };
+
   output.attribute_groups = R.filter(
     R.complement(R.isNil),
     R.concat(
@@ -251,10 +274,9 @@ const transformProduct = inputProduct => {
   );
 
   // Promotions
-
-  const processPromotions = R.pipe(
-    R.map(promotion => {
-      // Build Links
+  const processPromotion = R.pipe(
+    // Build Links
+    promotion => {
       return R.pipe(
         R.pick(['condition_link', 'url', 'promo_link']),
         R.mapObjIndexed(generateLinks),
@@ -262,9 +284,9 @@ const transformProduct = inputProduct => {
         R.assoc('links', R.__, promotion),
         R.omit(['condition_link', 'url', 'promo_link'])
       )(promotion);
-    }),
-    R.map(promotion => {
-      // Build Media
+    },
+    // Build Media
+    promotion => {
       return R.pipe(
         R.pick(['image_url', 'image', 'image_links']),
         R.mapObjIndexed(generateLinks),
@@ -272,16 +294,15 @@ const transformProduct = inputProduct => {
         R.assoc('media', R.__, promotion),
         R.omit(['image_url', 'image', 'image_links'])
       )(promotion);
-    }),
-    R.map(promotion => {
-      // Adapt products
+    },
+    // Adapt products
+    promotion => {
       promotion.products = R.when(
         R.complement(R.isNil),
         R.map(transformProduct)
       )(promotion.products);
       return promotion;
-    }),
-    R.map(R.reject(R.isEmpty))
+    }
   );
 
   const buildPromotions = R.pipe(
@@ -293,9 +314,16 @@ const transformProduct = inputProduct => {
     ]),
     R.values,
     R.flatten,
-    processPromotions
+    R.map(processPromotion),
+    R.map(R.reject(R.isEmpty))
   );
   output.promotions = buildPromotions(input);
+
+  // Signal Services
+  output.services = R.map(
+    R.evolve({ installation: processPromotion }),
+    R.propOr([], 'signal_services', input)
+  );
 
   // Variants
   // Variants :: Media
@@ -324,7 +352,10 @@ const transformProduct = inputProduct => {
   const variantKeysTransformation = {
     color: R.pipe(
       R.pick(['color', 'image_color']),
-      renameKeys({ image_color: 'image', color: 'value' })
+      renameKeys({
+        image_color: 'image',
+        color: 'value'
+      })
     ),
     sizes: sku =>
       R.filter(R.identity, {
@@ -345,7 +376,7 @@ const transformProduct = inputProduct => {
     format: R.prop('format')
   };
 
-  output.variants = input.skus.map(skuObject => {
+  const transformSku = skuObject => {
     let variant = R.pick(
       [
         'sku',
@@ -353,14 +384,18 @@ const transformProduct = inputProduct => {
         'internal_id',
         'ean_provider',
         'merchandising',
-        'web_model_code'
+        'web_model_code',
+        'mandatory_info',
+        'units',
+        'marketplace_info'
       ],
       skuObject
     );
-    variant.cross_selling = R.when(
-      R.complement(R.isNil),
+
+    variant.cross_selling = R.pipe(
+      R.propOr([], ['cross_selling']),
       R.map(transformProduct)
-    )(skuObject.cross_selling);
+    )(skuObject);
 
     variant.attributes = R.pipe(
       R.propOr([], 'house_hold'),
@@ -371,7 +406,11 @@ const transformProduct = inputProduct => {
       R.mapObjIndexed((value, key) => {
         return {
           name: key,
-          values: [{ value }]
+          values: [
+            {
+              value
+            }
+          ]
         };
       }),
       R.values,
@@ -387,15 +426,26 @@ const transformProduct = inputProduct => {
       components: skuObject.bundle_components
     };
 
+    variant.services = R.propOr([], skuObject.signal_services);
+    R.map(
+      R.pipe(
+        R.propOr([], 'installation')
+        // TODO adapt products
+      ),
+      variant.services
+    );
+
     variant.services = R.map(
-      R.evolve({ installation: R.pipe(R.of, processPromotions) }),
+      R.evolve({ installation: processPromotion }),
       R.propOr([], 'signal_services', skuObject)
     );
 
     variant.variant_keys = R.map(
       variantKey =>
         R.merge(
-          { key: variantKey },
+          {
+            key: variantKey
+          },
           variantKeysTransformation[variantKey](skuObject)
         ),
       R.pipe(R.pick(R.keys(variantKeysTransformation)), R.keys)(skuObject)
@@ -420,14 +470,9 @@ const transformProduct = inputProduct => {
     ])(input);
 
     return R.reject(R.isEmpty, variant);
-  });
+  };
 
-  // Signal services
-
-  output.services = R.map(
-    R.evolve({ installation: R.pipe(R.of, processPromotions) }),
-    R.propOr([], 'signal_services', input)
-  );
+  output.variants = input.skus.map(transformSku);
 
   // Providers
   const providersMap = {
@@ -448,8 +493,16 @@ const transformProduct = inputProduct => {
         R.propOr([], 'marketplace_providers_details', input)
       ),
     R.filter(R.identity),
-    R.map(renameKeys({ provider_id: 'id' })),
-    R.map(R.evolve({ id: R.when(R.is(Number), R.toString) }))
+    R.map(
+      renameKeys({
+        provider_id: 'id'
+      })
+    ),
+    R.map(
+      R.evolve({
+        id: R.when(R.is(Number), R.toString)
+      })
+    )
   )(input);
 
   // Offers
@@ -458,7 +511,15 @@ const transformProduct = inputProduct => {
   const validShippingOptions = R.pipe(
     R.converge(R.concat, [
       R.propOr([], 'sts_companies'),
-      R.ifElse(R.has('home_delivery'), R.always([{ id: '000' }]), R.always([]))
+      R.ifElse(
+        R.has('home_delivery'),
+        R.always([
+          {
+            id: '000'
+          }
+        ]),
+        R.always([])
+      )
     ]),
     R.pluck('id')
   );
@@ -522,7 +583,9 @@ const transformProduct = inputProduct => {
           ),
           R.when(
             R.always(R.prop('available_centre', sku.badges)),
-            R.append({ name: 'eci_pickup' })
+            R.append({
+              name: 'eci_pickup'
+            })
           ),
           R.when(
             R.find(R.propEq('id', '000')),
@@ -553,7 +616,11 @@ const transformProduct = inputProduct => {
       ),
       R.pipe(
         R.filter(R.path(['prices', 'length'])),
-        R.map(R.evolve({ prices: R.map(R.omit(['express_checkout'])) }))
+        R.map(
+          R.evolve({
+            prices: R.map(R.omit(['express_checkout']))
+          })
+        )
       )
     )(offer);
   };
@@ -569,12 +636,24 @@ const transformProduct = inputProduct => {
 
   output.offers = R.chain(skuObject => {
     return R.pipe(
+      R.converge(R.concat, [R.propOr([], 'providers'), R.of]),
       R.map(provider => R.merge(provider, provider.offer)),
-      R.map(provider => ({
-        offer_id: R.prop('offer_id', provider),
-        provider_id: R.propOr(output.default_provider, 'provider_id', provider),
-        variant: R.pick(['sku'], skuObject),
-        pricing: R.pick(
+      R.map(providedSku => {
+        let offer = {};
+        offer.provider = R.propOr(
+          output.default_provider,
+          'provider_id',
+          providedSku
+        );
+        offer.id = R.propOr(
+          `internal_${offer.provider}_${skuObject.sku}`,
+          'offer_id',
+          providedSku
+        );
+        offer.variant = R.prop('sku', skuObject);
+
+        // Offer :: Pricing
+        offer.pricing = R.pick(
           [
             'price',
             'sale_price',
@@ -582,9 +661,17 @@ const transformProduct = inputProduct => {
             'promotional_price',
             'unit_price'
           ],
-          provider
-        ),
-        availability: R.merge(
+          providedSku
+        );
+
+        // Offer :: Shipping Options
+        offer.shipping_options = getShippingOptionsForOffer(
+          providedSku,
+          skuObject
+        );
+
+        // Offer :: Availability
+        offer.availability = R.merge(
           {
             status: R.ifElse(R.equals('ADD'), R.always('available'), R.toLower)(
               skuObject.add_to_cart
@@ -592,23 +679,18 @@ const transformProduct = inputProduct => {
             form: skuObject.form_name,
             edition_date: skuObject.edition_date,
             unpublished_date: skuObject.unpublished_date,
-            with_stock: provider.delivery_time_with_stock,
-            without_stock: provider.delivery_time_without_stock,
+            with_stock: providedSku.delivery_time_with_stock,
+            without_stock: providedSku.delivery_time_without_stock,
             without_stock_date: skuObject.without_basket_date
           },
           R.pick(availabilityBadges, R.propOr({}, 'badges', skuObject))
-        )
-      })),
-      R.map(offer => {
-        return R.merge(offer, {
-          shipping_options: getShippingOptionsForOffer(offer, skuObject)
-        });
-      }),
-      R.map(R.filter(R.identity))
-      // R.filter(R.prop('provider_id')) // Don't remember why this is here, but probably shouldn't
-    )(R.concat(R.propOr([], 'providers', skuObject), [skuObject]));
+        );
+        return offer;
+      })
+    )(skuObject);
   }, input.skus);
-  return cleanObject(output);
+
+  return output;
 };
 
 module.exports = transformProduct;
